cdeabec06a2d2788eab517b855b63f8d951f52f9
sezerender185-spec <sezerender185@gmail.com>
2025-11-07 00:04:00 +0300

    Add files via upload

---
+++ Week_6_Task_9.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+const int π = 3.14;
+class Circle {
+private:
+    double radius;
+public:
+    Circle(double rad) {
+        radius = rad;
+    }
+    double getRadius() {
+        return radius;
+    }
+    void printCircleInfo(Circle c) {
+        double rad = c.getRadius();
+        double area = π * pow(rad,rad);
+        cout << "Radius: " << rad << endl;
+        cout << "Area: " << area;
+
+
+    }
+    
+};
+
+
+int main()
+{
+    Circle circle(3.1);
+    circle.printCircleInfo(circle);
+
+    return 0;
+}
+
